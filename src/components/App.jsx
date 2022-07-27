9ce5e66eb6dcabf047fdd59466e553009c2510c2
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-07-27 14:00:28 +0300

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
 import { Profile } from "components/Profile/Profile";
 import user from "../dataFiles/user.json"
+import { Statistics } from "components/Statistics/Statistics"
+import data from "../dataFiles/data.json"
 
 
 
 export const App = () => {
   return (
-    <Profile
-      key={user.username}
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    />
+    <div>
+      <Profile
+        key={user.username}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
